7413b3a91766fc7d34e8a3a540bbe821719b2a81
Tynan Ford <tford@lbl.gov>
2020-06-23 22:17:58 -0700

    update pip package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="phoebusgen",
-    version="0.0.2",
+    version="0.0.3",
     author="Tynan Ford",
     author_email="tford@lbl.gov",
     description="Control screen generator for Phoebus",
