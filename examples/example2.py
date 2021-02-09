57dad3b3d93aba044d82d409ed16ced8097a5a2e
Tynan Ford <tford@lbl.gov>
2021-02-09 11:05:15 -0800

    update examples

--- examples/example2.py
+++ examples/example2.py
@@ -1,27 +1,28 @@
 from phoebusgen import widget as w
 from phoebusgen import screen as s
+from phoebusgen import colors, fonts
 
 widget_height = 20
 widget_length = 120
 
 def make_group(num_pvs):
     group = w.Group('Example Group', 0, 0, 400, 240)
-    group.predefined_font('Header 2')
+    group.predefined_font(fonts.Header2)
     for i in range(1, num_pvs+1):
         y_position = (i-1)*widget_height
         pv_name = '$(P):update{}<VInteger>({})'.format(i, i*100)
         label = w.Label('Label{}'.format(i), 'Here is a Label', 0, y_position, widget_length, widget_height)
         text_update = w.TextUpdate('TextUpdate{}'.format(i), pv_name, widget_length, y_position, widget_length, widget_height)
         comment = w.Label('Comment1'.format(i), 'Label explanation', widget_length*2, y_position, widget_length, widget_height)
-        comment.predefined_font('Comment')
+        comment.predefined_font(fonts.Comment)
         comment.font_style_bold_italic()
         label.foreground_color(100, 0, 100)
-        comment.predefined_foreground_color('Attention')
+        comment.predefined_foreground_color(colors.Attention)
         group.add_widget([label, text_update, comment])
     return group
 
 def make_screen():
-    example_screen = s.Screen('Phoebusgen Example 3', './example3.bob')
+    example_screen = s.Screen('Phoebusgen Example 2', './example2.bob')
     example_screen.width(1000)
     example_screen.height(1000)
     example_screen.macro('P', 'loc://example3')
