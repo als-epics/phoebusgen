fc96a57eb50a81d1dcb232226d6397205cf89a03
Tynan Ford <tford@lbl.gov>
2021-08-25 20:42:10 -0400

    update to version 1.0.0 in pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="phoebusgen",
-    version="0.1.0",
+    version="1.0.0",
     author="Tynan Ford",
     author_email="tford@lbl.gov",
     description="Control screen generator for Phoebus",
@@ -18,7 +18,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Topic :: Software Development :: Code Generators"
     ],
     python_requires='>=3.5',
