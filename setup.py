9c36c91336b11336c3f109008ad98586c8ee08ce
Tynan Ford <tford@lbl.gov>
2021-09-23 10:21:10 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="phoebusgen",
-    version="1.0.0",
+    version="1.0.1",
     author="Tynan Ford",
     author_email="tford@lbl.gov",
     description="Control screen generator for Phoebus",
