28dc2922314058ef60998e2927d3dc9aba6a4704
Tynan Ford <tford@lbl.gov>
2021-11-02 12:17:28 -0700

    add phoebusgen called inside phoebus screen example

---
+++ examples/exampleInPhoebus.py
@@ -0,0 +1,18 @@
+import sys
+import phoebusgen
+
+pv_arg = sys.argv[1]
+
+print("Received the following arg from phoebus: {}".format(pv_arg))
+
+my_screen = phoebusgen.screen.Screen("My Generated Screen", "/home/tford/als/hlc/phoebusgen/examples/generated.bob")
+
+y = 0
+for i in range(int(pv_arg)):
+    idx = str(i)
+    label = phoebusgen.widget.Label("label{}".format(idx), "Label {}".format(idx), 0, y, 100, 20)
+    my_screen.add_widget(label)
+    y += 30
+
+my_screen.write_screen()
+
