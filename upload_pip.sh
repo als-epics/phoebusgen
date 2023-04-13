174ea982273b444d5063ebe94a2b5be002345a66
Tynan Ford <tford@lbl.gov>
2023-04-13 16:55:16 -0700

    use proper pip upload action

--- upload_pip.sh
+++ upload_pip.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-rm -rf build
-rm -rf phoebusgen.egg-info
-rm -rf dist
-python3 setup.py sdist bdist_wheel
-python3 -m twine upload dist/* --verbose
+printf "\n##################################"
+printf "\nThis is done in github actions now\n"
+printf "##################################\n\n"
+#rm -rf build
+#rm -rf phoebusgen.egg-info
+#rm -rf dist
+#python3 setup.py sdist bdist_wheel
+#python3 -m twine upload dist/* --verbose
